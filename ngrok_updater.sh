87ff21b8d6699728bf624f3e31fbbad3505d10e6
donaldsimpson@gmail.com <donaldsimpson@gmail.com>
2019-09-26 13:53:49 +0100

    cleanup

--- ngrok_updater.sh
+++ ngrok_updater.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Backup of the Jenkin job/script I put together to automatically update my home ngrok tunnel.
+# Backup of the Jenkins job/script I put together to automatically update my home ngrok tunnel.
 # When the tunnel dies, this script will (via Jenkins) create a new one and update a PHP redirect file on my
 # AWS Host that allows me to connect to my CGNET'd home server via my AWS website using a dynamic ngrok end point
-# It's a little bit mental, but it's worked without a hitch for 6 months and counting :-)
 # Uses:
 # - Jenkins
 # - bash
@@ -15,30 +14,28 @@
 # - AWS
 
 
-# check if ngrok is running
+# check if ngrok is running/not
 pidof  ngrok >/dev/null
 if [[ $? -ne 0 ]] ; then
-		# A restart and update is required
-        echo "Restarting ngrok on $(date)"
-        # Start up a new instance of ngrok
-        BUILD_ID=dontKillMe nohup /root/ngrok/ngrok http -region eu 80 &
-		# Give it a moment
+		# A (re)start and update is required
+    echo "Starting ngrok on $(date)"
+    # Start up a new instance of ngrok
+    BUILD_ID=dontKillMe nohup /root/ngrok/ngrok http -region eu 80 &
+		# Give it a moment before testing it...
 		echo "Sleeping for 15 seconds..."
-        sleep 15
-        # Get the updated publish_url value from the ngrok api
+    sleep 15
+    # Get the updated publish_url value from the ngrok api
 		export NGROKURL=`curl -s http://127.0.0.1:4040/api/tunnels | jq '.' | grep public_url | grep https | awk -F\" '{print $4}'`
-        echo "NGROKURL is $NGROKURL"
-        # add that to a one-line PHP redirect page
+    echo "NGROKURL is $NGROKURL"
+    # add that to a one-line PHP redirect page
 		echo "<?php header('Location: $NGROKURL/zm'); exit;?>" > zm.php
-        # upload that to my AWS host
-        echo "scp'ing file to AWS host..."
-		scp -i /hMY_AWS_KEY_FILE.pem zm.php MY_AWS_USER@MY_AWS_HOST.amazonaws.com:/MY_HTDOCS_DIR/ZoneMinder.php 
+    # upload that to my AWS host
+    echo "scp'ing zm.php to AWS host..."
+		scp -i /MY_AWS_KEY_FILE.pem zm.php MY_AWS_USER@MY_AWS_HOST.amazonaws.com:/MY_HTDOCS_DIR/ZoneMinder.php
 		echo "Transfer complete."
-        # Send an update message via email
+    # Send an update message via email
 		echo "New ngrok url is $NGROKURL/zm" | mailx -s "ngrok zm url updated" MY_EMAIL@gmail.com
 else
 		# Nothing needed, carry on
-		echo "ngrok is running, nothing to do"
+		echo "ngrok is currently running, nothing to do"
 fi
-
-
